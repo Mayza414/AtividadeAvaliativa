0fcbc080f839f348c6e5b36e91b0cff5cd7848c0
mayza <mayza.silva@unemat.br>
2024-03-17 15:10:50 -0400

    Entrega Atividade!

---
+++ app/src/main/java/br/com/mayza/atividadeavaliativa/MainActivity.java
@@ -0,0 +1,38 @@
+package br.com.mayza.atividadeavaliativa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText campoNome;
+    Button btnSaudacao;
+    TextView tvMensagem;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        campoNome = findViewById(R.id.editNome);
+        btnSaudacao = findViewById(R.id.btnBotao);
+        tvMensagem = findViewById(R.id.tvNome);
+
+        btnSaudacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //PEGAR A INFORMAÇÃO DO EDITTEXT
+                String nome = campoNome.getText().toString();
+                String mensagem = "Olá! " + nome;
+                tvMensagem.setText(mensagem);
+
+            }
+        });
+    }
+}
